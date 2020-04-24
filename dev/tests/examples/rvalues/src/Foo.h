da345bdb798a48f9a2e4a60f9d7959391bf33d22
Jefferson M Fujioka <j.fujioka@dac.eu>
2020-04-24 15:53:25 +0200

    tests: add stub for tests/examples/rvalue

---
+++ dev/tests/examples/rvalues/src/Foo.h
@@ -0,0 +1,11 @@
+// TODO add Copyright
+
+#ifndef DEV_TESTS_EXAMPLES_RVALUES_SRC_FOOO_H
+#define DEV_TESTS_EXAMPLES_RVALUES_SRC_FOOO_H
+
+class Foo {
+public:
+    static int Sum(int a, int b);
+};
+
+#endif //DEV_TESTS_EXAMPLES_RVALUES_SRC_FOOO_H
