f736ab43601bb23550cdb37a1f39f6a7c8b7d35b
Jefferson M Fujioka <j.fujioka@dac.eu>
2020-05-14 17:07:39 +0200

    feat(apps/gdb-py): app to demostrate how to use gbd-py

---
+++ dev/apps/gdb-py/resources/run_gdbpy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# TODO remove absolute paths
+gdb --batch \
+    -x "~/Projects/japonaldo/cpp_studies/dev/apps/gdb-py/resources/test.gdb" \
+    /home/fujioka/Projects/japonaldo/cpp_studies/.build/dev/apps/gdb-py/src/gdb-py 
