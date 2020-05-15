2739c1f5c4d5173368110987ec988a7a2bf31184
Jefferson M Fujioka <jefferson.fujioka@gmail.com>
2020-05-15 16:50:40 +0200

    chore(build): add configure.sh to setup the build environment/commands/aliases

---
+++ scripts/targets/fujioka-x86_64-linux-gnu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export JMF_TARGET_NAME="cpp_studies"
+export JMF_TARGET_PLAT="x86_64"
+export JMF_TARGET_SUPPLIER="Linux"
+export JMF_COMPILER_PREFIX="x86_64-linux-gnu"
+export JMF_TARGET_NAME_PLAT=${JMF_TARGET_NAME}_${JMF_TARGET_PLAT}
