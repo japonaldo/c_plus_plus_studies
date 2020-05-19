0ccee9c1bbd05b6a2ecfe19adb2c185c99944ce1
Jefferson M Fujioka <jefferson.fujioka@gmail.com>
2020-05-19 16:47:43 +0200

    tests(examples) add stub for Variadic Templates tests

---
+++ dev/tests/examples/variadic-templates/src/VariadicTemplate_Test.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+
+/*
+ * This code stem from:
+ * 1: https://eli.thegreenplace.net/2014/variadic-templates-in-c/
+ * 2: https://www.modernescpp.com/index.php/c-insights-variadic-templates
+ */
+
+namespace jmf
+{
+namespace tests
+{
+namespace examples
+{
+
+// TODO
+
+}
+}
+}
