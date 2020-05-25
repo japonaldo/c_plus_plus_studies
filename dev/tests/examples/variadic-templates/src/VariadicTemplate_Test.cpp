095670e088d29414c975d4a373114e11d8dbe0f0
Jefferson M Fujioka <jefferson.fujioka@gmail.com>
2020-05-25 14:42:54 +0200

    tests(variadic): add ComparePair test cases

--- dev/tests/examples/variadic-templates/src/VariadicTemplate_Test.cpp
+++ dev/tests/examples/variadic-templates/src/VariadicTemplate_Test.cpp
@@ -112,7 +112,66 @@ TEST(VariadicTemplate_Test, AddAnyInt)
   EXPECT_EQ(AddAny(1.5, 1, 2, 4.5, 3.5), 12.5);
 }
 
+template<typename T>
+bool ComparePair(T a)
+{
+  return false;
+}
 
+template<typename T>
+bool ComparePair(T a, T b)
+{
+  return a == b;
+}
+
+template<typename T, typename ... Args>
+bool ComparePair(T a, T b, Args ... args)
+{
+  return (a == b) && ComparePair(args...);
+}
+
+TEST(VariadicTemplate_Test, ComparePairIntFalses)
+{
+  EXPECT_EQ(ComparePair(0), false);
+  EXPECT_EQ(ComparePair(0, 1), false);
+  EXPECT_EQ(ComparePair(1, 1, 2), false);
+  EXPECT_EQ(ComparePair(1, 1, 2, 5), false);
+  EXPECT_EQ(ComparePair(1, 1, 2, 2, 6, 3), false);
+}
+
+TEST(VariadicTemplate_Test, ComparePairExpectIntTrues)
+{
+  EXPECT_EQ(ComparePair(1, 1), true);
+  EXPECT_EQ(ComparePair(1, 1, 2, 2), true);
+  EXPECT_EQ(ComparePair(1, 1, 2, 2, 4, 4), true);
+}
+
+TEST(VariadicTemplate_Test, ComparePairDoubleFalses)
+{
+  EXPECT_EQ(ComparePair(0.0), false);
+  EXPECT_EQ(ComparePair(0.75, 1.75), false);
+  EXPECT_EQ(ComparePair(1.75, 1.75, 2.99), false);
+  EXPECT_EQ(ComparePair(1.75, 1.75, 2.99, 5.99), false);
+  EXPECT_EQ(ComparePair(1.75, 1.75, 2.99, 2.99, 6.123, 3.123), false);
+}
+
+TEST(VariadicTemplate_Test, ComparePairExpectDoubleTrues)
+{
+  EXPECT_EQ(ComparePair(1.55, 1.55), true);
+  EXPECT_EQ(ComparePair(1.55, 1.55, 2.99, 2.99), true);
+  EXPECT_EQ(ComparePair(1.55, 1.55, 2.99, 2.99, 4.123, 4.123), true);
+}
+
+TEST(VariadicTemplate_Test, ComparePairExpectIntVsDouble)
+{
+  // the following doesn't compile, gcc gives the following error message:
+  // error: no matching function for call to ‘ComparePair(int, double)’
+  // EXPECT_EQ(ComparePair(1, 1.6), true);
+
+  // However, the following compiles and the EXPECT_EQ passes
+  // because 1.6 is being implicitly converted to int
+  EXPECT_EQ(ComparePair<int>(1, 1.6), true);
+}
 
 }
 }
